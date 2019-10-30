a31e7f6b12ef63747ca4c5278d745a45192ebec4
Yancysoul <zhengyong9709@163.com>
2019-10-30 11:24:56 +0800

    修改动态路由获取

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,12 +1,11 @@
 import { getToken, setToken, removeToken } from '@/utils/auth'
-import { setStore, getStore, removeStore } from '@/utils/store'
 import { setRouter } from '@/utils/router'
 import router from '@/router'
 
 const user = {
   state: {
     token: getToken(),
-    menus: getStore({name: 'menus'})
+    menus: []
   },
   mutations: {
     SET_TOKEN: (state, token) => {
@@ -16,11 +15,6 @@ const user = {
     SET_MENU: (state, menus) => {
       var routerMenu = setRouter(menus)
       state.menus = routerMenu
-      setStore({
-        name: 'menus',
-        content: routerMenu,
-        type: true
-      })
       router.options.routes.push(...routerMenu)
     }
   },
